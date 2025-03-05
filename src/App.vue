41e453f8004dd5ea85d1327d4b86d14ffb2c5738
springlogger <vktortbin@gmail.com>
2025-03-05 11:12:26 +0300

    Minimal Electron+Vue+TS+TailwindCSS project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-3xl font-bold underline">💖 Hello World!</h1>
+    <p>Welcome to your Electron applicationfff.</p>
+  </template>
+  
+  <script setup>
+  console.log('👋 This message is being logged by "App.vue", included via Vite');
+  </script>
